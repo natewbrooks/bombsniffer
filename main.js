c3aef258faeec22fffad64c13262e2369cd297b9
nate <59595919+natewbrooks@users.noreply.github.com>
2022-10-24 12:45:30 -0400

    aesthetic changes

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ var mines = [];
 
 const MINES = 99;
 const FLAGS = MINES;
-const ROWS = 16;
-const COLUMNS = 30;
+const ROWS = 16; //16
+const COLUMNS = 30; //30
 
 const GAMEBOARD = document.querySelector(".board");
 const GAME_TILE_SIZE = 35;
@@ -12,7 +12,8 @@ const GAME_TILE_PADDING = 1;
 
 const UNCHECKED_COLOR = "darkgray";
 const CHECKED_COLOR = "white";
-const FLAG_COLOR = "darkred";
+const FLAG_URL = "images/flag_icon.png";
+const BOMB_URL = "images/bomb_icon.png";
 
 var flagsUsed = 0;
 
@@ -23,6 +24,7 @@ class Tile {
         this.mine = mine;
         this.number = 0;
         this.flagged = false;
+        this.clicked = false;
     }
 }
 
@@ -31,13 +33,24 @@ window.onload = main();
 window.onclick = e => {
     if(e.target.tile != null && !e.target.tile.flagged) {
         if(e.target.tile.mine) {
-            e.target.style.backgroundColor = "orange";
+            e.target.style.backgroundImage = "url("+ BOMB_URL +")";
             gameOver();
         } else {
             e.target.style.backgroundColor = CHECKED_COLOR;
+            e.target.tile.clicked = true;
             e.target.firstChild.innerHTML = e.target.tile.number;
             e.target.firstChild.style.display = "block";
         }
+
+        let pass = true;
+        board.forEach(tile => {
+            if (!tile.mine && !tile.clicked) {
+                pass = false;
+                return;
+            }
+        });
+        
+        if (pass) winnerwinnerchickendinner();
     }
 }
 
@@ -46,29 +59,31 @@ window.oncontextmenu = e => {
         if(e.target.tile.flagged) {
             e.target.tile.flagged = false;
             e.target.style.backgroundColor = UNCHECKED_COLOR;
+            e.target.style.backgroundImage = "none";
+            flagsUsed--;
             return;
-        }
-
-        if(flagsUsed < FLAGS) {
+        } else if (flagsUsed < FLAGS) {
             e.target.tile.flagged = true;
-            e.target.style.backgroundColor = FLAG_COLOR;
+            e.target.style.backgroundImage = "url("+ FLAG_URL +")";
+            flagsUsed++;
             return;
         }
 
+        if(flagsUsed == FLAGS) {
+            let pass = true;
+            mines.forEach(mine => {
+                if(!mine.flagged) {
+                    pass = false;
+                }
+            });
+    
+            if(pass) winnerwinnerchickendinner();
+        }
 
         console.log("You don't have any flags left!");
     }
 
-    if(flagsUsed == FLAGS) {
-        let pass = true;
-        mines.forEach(mine => {
-            if(!mine.flagged) {
-                pass = false;
-            }
-        });
-
-        if(pass) winnerwinnerchickendinner();
-    }
+    
 }
 
 function main() {
